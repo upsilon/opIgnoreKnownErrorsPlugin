c302ffee391dd48d64cf76f04abd7cee1533b77d
Kimura Youichi <kim.upsilon@bucyou.net>
2016-11-15 16:05:09 +0900

    既にset_error_handlerに設定されているハンドラを考慮する

--- config/opIgnoreKnownErrorsPluginConfiguration.class.php
+++ config/opIgnoreKnownErrorsPluginConfiguration.class.php
@@ -4,15 +4,27 @@ require_once dirname(__FILE__).'/../lib/util/opIgnoreKnownErrors.class.php';
 
 class opIgnoreKnownErrorsPluginConfiguration extends sfPluginConfiguration
 {
+  private $origHandler;
+
   public function configure()
   {
     opIgnoreKnownErrors::loadConfig(dirname(__FILE__).'/known_errors.yml');
 
-    set_error_handler(array($this, 'errorHandler'));
+    $this->origHandler = set_error_handler(array($this, 'errorHandler'));
   }
 
   public function errorHandler($errno, $errstr, $errfile, $errline)
   {
-    return opIgnoreKnownErrors::isMatch($errno, $errstr, $errfile, $errline);
+    if (opIgnoreKnownErrors::isMatch($errno, $errstr, $errfile, $errline))
+    {
+      return true;
+    }
+
+    if (null === $this->origHandler)
+    {
+      return false;
+    }
+
+    return call_user_func($this->origHandler, $errno, $errstr, $errfile, $errline);
   }
 }
